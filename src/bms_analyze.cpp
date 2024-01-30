c1c4a87fbdbaa0370472529faa253d42f6472972
Eisenwave <me@eisenwave.net>
2024-01-30 17:24:23 +0100

    add lookup stack to all blocks

--- src/bms_analyze.cpp
+++ src/bms_analyze.cpp
@@ -24,35 +24,45 @@ Node_Handle get_bit_generic_expression(Type_Node& type)
 struct Symbol_Table {
 private:
     using map_type = std::unordered_map<std::string_view, ast::Node_Handle>;
-    map_type symbols;
-    Symbol_Table* parent = nullptr;
+    map_type m_symbols;
+    Symbol_Table* m_parent = nullptr;
+    std::vector<Symbol_Table> m_children;
 
 public:
     Symbol_Table() = default;
 
-    Symbol_Table(Symbol_Table& parent)
-        : parent(&parent)
+    Symbol_Table& push()
     {
+        Symbol_Table& result = m_children.emplace_back();
+        result.m_parent = this;
+        return result;
     }
 
-    std::pair<map_type::iterator, bool> emplace(std::string_view symbol, ast::Node_Handle node)
+    std::variant<map_type::iterator, Node_Handle> emplace(std::string_view symbol,
+                                                          ast::Node_Handle node)
     {
-        if (parent != nullptr) {
-            if (auto iter = parent->symbols.find(symbol); iter != parent->symbols.end()) {
-                return { iter, false };
+        if (m_parent != nullptr) {
+            if (std::optional<Node_Handle> old = m_parent->find(symbol)) {
+                return *old;
             }
         }
-        return symbols.emplace(symbol, node);
+        auto [iter, success] = m_symbols.emplace(symbol, node);
+        if (success) {
+            return iter;
+        }
+        else {
+            return iter->second;
+        }
     }
 
     std::optional<Node_Handle> find(std::string_view symbol)
     {
-        if (parent != nullptr) {
-            if (auto parent_result = parent->find(symbol)) {
+        if (m_parent != nullptr) {
+            if (auto parent_result = m_parent->find(symbol)) {
                 return parent_result;
             }
         }
-        if (auto iter = symbols.find(symbol); iter != symbols.end()) {
+        if (auto iter = m_symbols.find(symbol); iter != m_symbols.end()) {
             return iter->second;
         }
         return std::nullopt;
@@ -63,8 +73,7 @@ struct Analyzer {
 private:
     Parsed_Program& m_program;
     Program_Node& m_root;
-    Symbol_Table m_global_symbols;
-    std::unordered_map<std::string_view, Symbol_Table> m_function_tables;
+    Symbol_Table m_symbols;
 
 public:
     Analyzer(Parsed_Program& program)
@@ -113,11 +122,11 @@ private:
     Analysis_Result analyze_symbols_global(Node_Handle handle, Let_Const_Node& n)
     {
         BIT_MANIPULATION_ASSERT(handle != Node_Handle::null);
-        auto [old, success] = m_global_symbols.emplace(n.name, handle);
-        if (!success) {
+        auto it_or_handle = m_symbols.emplace(n.name, handle);
+        if (Node_Handle* old = std::get_if<Node_Handle>(&it_or_handle)) {
             return { .code = Analysis_Error_Code::failed_to_define_global_const,
                      .fail_token = n.token,
-                     .cause_token = get_token(get_node(old->second)) };
+                     .cause_token = get_token(get_node(*old)) };
         }
         return Analysis_Result::ok;
     }
@@ -126,16 +135,13 @@ private:
     Analysis_Result analyze_symbols_global(Node_Handle handle, Function_Node& n)
     {
         BIT_MANIPULATION_ASSERT(handle != Node_Handle::null);
-        auto [old, success] = m_global_symbols.emplace(n.name, handle);
-        if (!success) {
+        auto it_or_handle = m_symbols.emplace(n.name, handle);
+        if (Node_Handle* old = std::get_if<Node_Handle>(&it_or_handle)) {
             return { .code = Analysis_Error_Code::failed_to_define_function,
                      .fail_token = n.token,
-                     .cause_token = get_token(get_node(old->second)) };
+                     .cause_token = get_token(get_node(*old)) };
         }
-        auto [table_pos, table_success] = m_function_tables.emplace(n.name, m_global_symbols);
-        BIT_MANIPULATION_ASSERT(table_success);
-
-        return analyze_symbols_local(handle, table_pos->second, n);
+        return analyze_symbols_local(handle, m_symbols.push(), n);
     }
 
     Analysis_Result analyze_symbols_local(Node_Handle handle, Symbol_Table& table)
@@ -169,11 +175,11 @@ private:
     Analysis_Result
     analyze_symbols_local(Node_Handle handle, Symbol_Table& table, Parameter_Node& node)
     {
-        auto [old, success] = table.emplace(node.name, handle);
-        if (!success) {
+        auto it_or_handle = m_symbols.emplace(node.name, handle);
+        if (Node_Handle* old = std::get_if<Node_Handle>(&it_or_handle)) {
             return { .code = Analysis_Error_Code::failed_to_define_parameter,
                      .fail_token = node.token,
-                     .cause_token = get_token(get_node(old->second)) };
+                     .cause_token = get_token(get_node(*old)) };
         }
         auto& type_node = std::get<Type_Node>(get_node(node.get_type()));
         Node_Handle g = get_bit_generic_expression(type_node);
@@ -191,6 +197,13 @@ private:
             }
         }
         return Analysis_Result::ok;
+    }
+
+    template <>
+    Analysis_Result
+    analyze_symbols_local(Node_Handle, Symbol_Table& table, Block_Statement_Node& node)
+    {
+        return analyze_all_symbols_local(node.get_children(), table.push());
     }
 
     template <>
