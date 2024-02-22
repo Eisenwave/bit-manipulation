5af1cb8c80da86b62f0a4ccd42977f3106d76dce
Eisenwave <me@eisenwave.net>
2024-02-22 23:29:45 +0100

    expand HTML_Writer

--- src/bmd/html_writer.cpp
+++ src/bmd/html_writer.cpp
@@ -4,73 +4,172 @@
 
 namespace bit_manipulation::bmd {
 
-HTML_Writer::HTML_Writer(std::ostream& out)
+HTML_Writer::HTML_Writer(std::ostream& out, const Config& config)
     : m_out(out)
+    , m_indent_width(config.indent_width)
 {
     BIT_MANIPULATION_ASSERT(!out.fail());
 }
 
-auto HTML_Writer::write(std::string_view raw) -> Self&
+HTML_Writer::~HTML_Writer()
 {
-    m_out << raw;
+    BIT_MANIPULATION_ASSERT(m_depth == 0);
+    BIT_MANIPULATION_ASSERT(m_state == State::normal);
+}
+
+void HTML_Writer::write(std::string_view raw)
+{
+    m_out.write(raw.data(), static_cast<std::streamsize>(raw.size()));
+}
+
+void HTML_Writer::write(char c)
+{
+    m_out.put(c);
+}
+
+void HTML_Writer::write_indent(Size level)
+{
+    char restore_fill = m_out.fill(' ');
+    m_out.width(level * m_indent_width);
+    m_out << "";
+    m_out.fill(restore_fill);
+}
+
+auto HTML_Writer::write_preamble(std::string_view tag) -> Self&
+{
+    BIT_MANIPULATION_ASSERT(m_state == State::initial);
+    m_state = State::normal;
+    write("<!DOCTYPE html>");
     return *this;
 }
 
-auto HTML_Writer::write(char c) -> Self&
-{
-    m_out << c;
-    return *this;
-}
-
-auto HTML_Writer::write_empty_tag_no_attributes(std::string_view tag) -> Self&
+auto HTML_Writer::write_empty_tag(std::string_view tag, HTML_Tag_Type type) -> Self&
 {
     BIT_MANIPULATION_ASSERT(m_state == State::normal);
     BIT_MANIPULATION_ASSERT(is_identifier(tag));
-    m_out << '<' << tag << "/>";
+    if (type == HTML_Tag_Type::block) {
+        write('\n');
+        write_indent(m_indent_depth);
+    }
+    write('<');
+    write(tag);
+    write("/>");
+
     return *this;
 }
 
-auto HTML_Writer::begin_tag_no_attributes(std::string_view tag) -> Self&
+auto HTML_Writer::begin_tag(std::string_view tag, HTML_Tag_Type type) -> Self&
 {
     BIT_MANIPULATION_ASSERT(m_state == State::normal);
     BIT_MANIPULATION_ASSERT(is_identifier(tag));
-    m_out << '<' << tag << '>';
+    if (type == HTML_Tag_Type::block) {
+        write('\n');
+        write_indent(m_indent_depth++);
+    }
+    ++m_depth;
+    write('<');
+    write(tag);
+    write('>');
+
     return *this;
 }
 
-auto HTML_Writer::begin_tag_with_attributes(std::string_view tag) -> Self&
+Attribute_Writer HTML_Writer::begin_tag_with_attributes(std::string_view tag, HTML_Tag_Type type)
 {
     BIT_MANIPULATION_ASSERT(m_state == State::normal);
     BIT_MANIPULATION_ASSERT(is_identifier(tag));
-    m_out << '<' << tag;
+    if (type == HTML_Tag_Type::block) {
+        write('\n');
+        write_indent(m_indent_depth++);
+    }
+    ++m_depth;
     m_state = State::attributes;
-    return *this;
+
+    write('<');
+    write(tag);
+
+    return { *this, type };
 }
 
-auto HTML_Writer::write_attribute(std::string_view key, std::string_view value) -> Self&
+auto HTML_Writer::end_tag(std::string_view tag, HTML_Tag_Type type) -> Self&
 {
-    BIT_MANIPULATION_ASSERT(m_state == State::attributes);
-    BIT_MANIPULATION_ASSERT(is_identifier(key));
-    m_out << ' ' << key << '=';
-    if (requires_quotes_in_attribute(value)) {
-        m_out << '"' << value << '"';
-    }
-    else {
-        m_out << value;
-    }
-    return *this;
-}
-
-auto HTML_Writer::close_tag(std::string_view tag) -> Self&
-{
+    BIT_MANIPULATION_ASSERT(m_state == State::normal);
     BIT_MANIPULATION_ASSERT(is_identifier(tag));
-    m_out << "</" << tag << '>';
+    BIT_MANIPULATION_ASSERT(m_depth != 0);
+
+    if (type == HTML_Tag_Type::block) {
+        BIT_MANIPULATION_ASSERT(m_indent_depth != 0);
+        write('\n');
+        write_indent(m_indent_depth--);
+    }
+    --m_depth;
+    m_state = State::normal;
+    write("</");
+    write(tag);
+    write('>');
+    return *this;
+}
+
+auto HTML_Writer::write_comment_tag(std::string_view comment, HTML_Tag_Type type) -> Self&
+{
+    BIT_MANIPULATION_ASSERT(m_state != State::attributes);
+    BIT_MANIPULATION_ASSERT(comment.find("-->") == std::string_view::npos);
+    if (type == HTML_Tag_Type::block) {
+        write('\n');
+        write_indent(m_indent_depth);
+    }
+    write("<!--");
+    write(comment);
+    write("-->\n");
+    return *this;
+}
+
+auto HTML_Writer::write_inner_text(std::string_view text) -> Self&
+{
+    BIT_MANIPULATION_ASSERT(m_state == State::normal);
+    BIT_MANIPULATION_ASSERT(text.find_first_of("<>") == std::string_view::npos);
+    m_state = State::normal;
+    write(text);
     return *this;
 }
 
 [[nodiscard]] HTML_Writer::operator bool() const
 {
     return bool(m_out);
+}
+
+auto HTML_Writer::write_attribute(std::string_view key, std::string_view value) -> Self&
+{
+    BIT_MANIPULATION_ASSERT(m_state == State::attributes);
+    BIT_MANIPULATION_ASSERT(is_identifier(key));
+    write(' ');
+    write(key);
+    write('=');
+    if (requires_quotes_in_attribute(value)) {
+        write('"');
+        write(value);
+        write('"');
+    }
+    else {
+        write(value);
+    }
+    return *this;
+}
+
+auto HTML_Writer::end_attributes(HTML_Tag_Type type) -> Self&
+{
+    BIT_MANIPULATION_ASSERT(m_state == State::attributes);
+    m_state = State::normal;
+    write(type == HTML_Tag_Type::block ? ">\n" : ">");
+    return *this;
+}
+
+auto HTML_Writer::end_empty_tag_attributes(HTML_Tag_Type type) -> Self&
+{
+    BIT_MANIPULATION_ASSERT(m_state == State::attributes);
+    m_state = State::normal;
+    write(type == HTML_Tag_Type::block ? "/>\n" : "/>");
+    return *this;
 }
 
 } // namespace bit_manipulation::bmd
--- src/bmd/html_writer.hpp
+++ src/bmd/html_writer.hpp
@@ -13,74 +13,114 @@ inline bool requires_quotes_in_attribute(std::string_view value)
     return value.find_first_of("\"'`=<> ") != std::string_view::npos;
 }
 
+enum struct HTML_Tag_Type { block, in_line };
+
+struct Attribute_Writer {
+private:
+    HTML_Writer& m_writer;
+    HTML_Tag_Type m_type;
+
+public:
+    Attribute_Writer(HTML_Writer& writer, HTML_Tag_Type type)
+        : m_writer(writer)
+        , m_type(type)
+    {
+    }
+
+    Attribute_Writer(const Attribute_Writer&) = delete;
+    Attribute_Writer& operator=(const Attribute_Writer&) = delete;
+
+    Attribute_Writer& write_attribute(std::string_view key, std::string_view value);
+
+    Attribute_Writer& end();
+    Attribute_Writer& end_empty();
+
+    ~Attribute_Writer();
+};
+
 /// @brief A class which provides member functions for writing HTML content to a stream
 /// correctly.
-/// This writer does not ensure that the HTML document is structurally correct.
-/// It only performs basic checks such as verifying that tag names are identifiers, etc.
-/// It also decides whether to use quotes or unquoted values for tag attributes.
+/// This writer only performs checks that are possibly without additional memory.
+/// These include:
+/// - verifying that given tag names and values are appropriate
+/// - ensuring that the number of opened tags matches the number of closed tags
+/// - ensuring that the preamble is written exactly once
 struct HTML_Writer {
 public:
     using Self = HTML_Writer;
 
+    struct Config {
+        Size indent_width;
+    };
+
 private:
     std::ostream& m_out;
-    enum struct State { normal, attributes } m_state = State::normal;
+    Size m_indent_width;
+    enum struct State { initial, normal, attributes } m_state = State::initial;
     Size m_depth = 0;
+    Size m_indent_depth = 0;
 
 public:
-    HTML_Writer(std::ostream& out);
+    HTML_Writer(std::ostream& out, const Config& config);
 
-    Self& write_preamble(std::string_view tag)
-    {
-        write("<!DOCTYPE html>\n");
-        return *this;
-    }
+    HTML_Writer(const HTML_Writer&) = delete;
+    HTML_Writer& operator=(const HTML_Writer&) = delete;
 
-    Self& write_empty_tag_no_attributes(std::string_view tag);
+    ~HTML_Writer();
 
-    Self& begin_tag_no_attributes(std::string_view tag);
+    Self& write_preamble(std::string_view tag);
 
-    Self& begin_tag_with_attributes(std::string_view tag);
+    Self& write_empty_tag(std::string_view tag, HTML_Tag_Type type);
 
-    Self& write_attribute(std::string_view key, std::string_view value);
+    Self& write_comment_tag(std::string_view comment, HTML_Tag_Type type);
 
-    Self& end_attributes()
-    {
-        m_state = State::normal;
-        write('>');
-        return *this;
-    }
+    Self& begin_tag(std::string_view tag, HTML_Tag_Type type);
 
-    Self& end_empty_tag_attributes()
-    {
-        m_state = State::normal;
-        write("/>");
-        return *this;
-    }
+    [[nodiscard]] Attribute_Writer begin_tag_with_attributes(std::string_view tag,
+                                                             HTML_Tag_Type type);
 
-    Self& close_tag(std::string_view tag);
+    Self& end_tag(std::string_view tag, HTML_Tag_Type type);
 
-    Self& write_inner_text(std::string_view text)
-    {
-        BIT_MANIPULATION_ASSERT(text.find_first_of("<>") == std::string_view::npos);
-        write(text);
-        return *this;
-    }
-
-    Self& write_inner_text_line(std::string_view text)
-    {
-        write_inner_text(text);
-        write('\n');
-        return *this;
-    }
+    Self& write_inner_text(std::string_view text);
 
     [[nodiscard]] operator bool() const;
 
 private:
-    Self& write(std::string_view raw);
+    friend struct Attribute_Writer;
 
-    Self& write(char c);
+    Self& write_attribute(std::string_view key, std::string_view value);
+    Self& end_attributes(HTML_Tag_Type type);
+    Self& end_empty_tag_attributes(HTML_Tag_Type type);
+
+    void write(std::string_view raw);
+    void write(char c);
+    void write_indent(Size level);
 };
+
+inline Attribute_Writer& Attribute_Writer::write_attribute(std::string_view key,
+                                                           std::string_view value)
+{
+    m_writer.write_attribute(key, value);
+    return *this;
+}
+
+inline Attribute_Writer& Attribute_Writer::end()
+{
+    m_writer.end_attributes(m_type);
+    return *this;
+}
+
+inline Attribute_Writer& Attribute_Writer::end_empty()
+{
+    m_writer.end_empty_tag_attributes(m_type);
+    return *this;
+}
+
+inline Attribute_Writer::~Attribute_Writer()
+{
+    // This indicates that end() or end_empty() weren't called.
+    BIT_MANIPULATION_ASSERT(m_writer.m_state != HTML_Writer::State::attributes);
+}
 
 } // namespace bit_manipulation::bmd
 
