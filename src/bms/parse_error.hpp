c39ca7661a303919644c846e4ba40b781161c813
Eisenwave <me@eisenwave.net>
2024-11-21 14:32:25 +0100

    [bms] add some documentation

--- src/bms/parse_error.hpp
+++ src/bms/parse_error.hpp
@@ -8,9 +8,17 @@
 
 namespace bit_manipulation::bms {
 
+/// @brief An error that occurs during parsing.
 struct Parse_Error {
+    /// @brief The rule that failed to match.
+    /// While there are multiple expansions, the BMS parser is completely deterministic given
+    /// arbitrary lookahead in a few isolated instances.
+    /// For example, if `let` appears in code, the rule to match would be `let_declaration`.
     Grammar_Rule fail_rule;
+    /// @brief A list of `Token_Type`s which, if they appeared, could have resulted in a match.
+    /// For example, following a `keyword_let`, this would include an `identifier`.
     std::span<const Token_Type> expected_tokens;
+    /// @brief The token at which parsing failed.
     Token fail_token;
 };
 
