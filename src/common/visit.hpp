346c4354414086adbb1ed44c61d3ce48659d39ff
Eisenwave <me@eisenwave.net>
2024-11-24 15:17:32 +0100

    [common] add visit case for 14 alternatives

--- src/common/visit.hpp
+++ src/common/visit.hpp
@@ -136,6 +136,31 @@ constexpr decltype(auto) fast_visit(F&& f, V&& v)
 }
 
 template <typename F, typename V>
+    requires(detail::variant_like_size_v<std::remove_cvref_t<V>> == 14)
+constexpr decltype(auto) fast_visit(F&& f, V&& v)
+{
+    if (v.valueless_by_exception()) {
+        throw std::bad_variant_access();
+    }
+    switch (v.index()) {
+        BIT_MANIPULATION_VISIT_CASE(0);
+        BIT_MANIPULATION_VISIT_CASE(1);
+        BIT_MANIPULATION_VISIT_CASE(2);
+        BIT_MANIPULATION_VISIT_CASE(3);
+        BIT_MANIPULATION_VISIT_CASE(4);
+        BIT_MANIPULATION_VISIT_CASE(5);
+        BIT_MANIPULATION_VISIT_CASE(6);
+        BIT_MANIPULATION_VISIT_CASE(7);
+        BIT_MANIPULATION_VISIT_CASE(8);
+        BIT_MANIPULATION_VISIT_CASE(9);
+        BIT_MANIPULATION_VISIT_CASE(10);
+        BIT_MANIPULATION_VISIT_CASE(11);
+        BIT_MANIPULATION_VISIT_CASE(13);
+    }
+    BIT_MANIPULATION_ASSERT_UNREACHABLE("impossible variant index");
+}
+
+template <typename F, typename V>
     requires(detail::variant_like_size_v<std::remove_cvref_t<V>> == 20)
 constexpr decltype(auto) fast_visit(F&& f, V&& v)
 {
@@ -230,6 +255,41 @@ constexpr decltype(auto) fast_visit(F&& f, V&& v)
         BIT_MANIPULATION_VISIT_CASE(19);
         BIT_MANIPULATION_VISIT_CASE(20);
         BIT_MANIPULATION_VISIT_CASE(21);
+    }
+    BIT_MANIPULATION_ASSERT_UNREACHABLE("impossible variant index");
+}
+
+template <typename F, typename V>
+    requires(detail::variant_like_size_v<std::remove_cvref_t<V>> == 23)
+constexpr decltype(auto) fast_visit(F&& f, V&& v)
+{
+    if (v.valueless_by_exception()) {
+        throw std::bad_variant_access();
+    }
+    switch (v.index()) {
+        BIT_MANIPULATION_VISIT_CASE(0);
+        BIT_MANIPULATION_VISIT_CASE(1);
+        BIT_MANIPULATION_VISIT_CASE(2);
+        BIT_MANIPULATION_VISIT_CASE(3);
+        BIT_MANIPULATION_VISIT_CASE(4);
+        BIT_MANIPULATION_VISIT_CASE(5);
+        BIT_MANIPULATION_VISIT_CASE(6);
+        BIT_MANIPULATION_VISIT_CASE(7);
+        BIT_MANIPULATION_VISIT_CASE(8);
+        BIT_MANIPULATION_VISIT_CASE(9);
+        BIT_MANIPULATION_VISIT_CASE(10);
+        BIT_MANIPULATION_VISIT_CASE(11);
+        BIT_MANIPULATION_VISIT_CASE(12);
+        BIT_MANIPULATION_VISIT_CASE(13);
+        BIT_MANIPULATION_VISIT_CASE(14);
+        BIT_MANIPULATION_VISIT_CASE(15);
+        BIT_MANIPULATION_VISIT_CASE(16);
+        BIT_MANIPULATION_VISIT_CASE(17);
+        BIT_MANIPULATION_VISIT_CASE(18);
+        BIT_MANIPULATION_VISIT_CASE(19);
+        BIT_MANIPULATION_VISIT_CASE(20);
+        BIT_MANIPULATION_VISIT_CASE(21);
+        BIT_MANIPULATION_VISIT_CASE(22);
     }
     BIT_MANIPULATION_ASSERT_UNREACHABLE("impossible variant index");
 }
