934398a5799e95857fd147293a24e68f199e83f0
Eisenwave <me@eisenwave.net>
2024-11-27 13:42:03 +0100

    [bms] remove redundant doc comments

--- src/bms/instantiate.cpp
+++ src/bms/instantiate.cpp
@@ -34,9 +34,6 @@ public:
     {
     }
 
-    /// @brief Instantiates every bit-generic function in the program using the same `Widths`.
-    /// This is mostly useful for testing purposes and cannot be triggered "naturally" through
-    /// implicit instantiations.
     Result<void, Analysis_Error> instantiate_all(const Widths& w)
     {
         for (ast::Some_Node* decl : get_children(*m_program.get_root())) {
@@ -50,18 +47,6 @@ public:
         return {};
     }
 
-    /// @brief Instantiates an individual function.
-    /// Contrary to `instantiate_all`, this may be necessitated by implicit instantiations,
-    /// for example when a function taking `Uint(N)` is called.
-    ///
-    /// At a high level, the process of instantiation works as follows:
-    ///   1. Make a `deep_copy` of the node to be instantiated
-    ///   2. `substitute_widths` to replace occurrences of bit-generic parameters with constants
-    ///   3. Append the resulting `ast::Function::Instance` to the existing node
-    /// @param h the pointer to the function node
-    /// @param node the function node
-    /// @param w the widths for instantiation
-    /// @return the instantiated instance, or `Analysis_Error`
     Result<const ast::Function::Instance*, Analysis_Error>
     instantiate_function(ast::Some_Node* h, ast::Function& node, const Widths& w)
     {
