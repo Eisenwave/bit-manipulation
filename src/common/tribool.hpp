d59cc793cd4537b4d5832a8d6db309bc57a703f5
Eisenwave <me@eisenwave.net>
2024-12-06 13:46:16 +0100

    [common] refactor Tribool to class

--- src/common/tribool.hpp
+++ src/common/tribool.hpp
@@ -1,93 +1,79 @@
 #ifndef BIT_MANIPULATION_TRIBOOL_HPP
 #define BIT_MANIPULATION_TRIBOOL_HPP
 
+#include "common/assert.hpp"
 #include "common/config.hpp"
 
 namespace bit_manipulation {
 
-enum struct Tribool : signed char { fawse = 0, twue = -1, maybe = 1 };
+struct Tribool {
+private:
+    signed char underlying;
 
-[[nodiscard]] [[gnu::always_inline]] constexpr Tribool to_tribool(bool x) noexcept
-{
-    return Tribool(-static_cast<signed char>(x));
-}
+    explicit constexpr Tribool(int x) noexcept
+        : underlying(x)
+    {
+    }
 
-[[nodiscard]] [[gnu::always_inline]] constexpr Tribool operator!(Tribool x) noexcept
-{
-    return x == Tribool::maybe ? Tribool::maybe : Tribool(~static_cast<signed char>(x));
-}
+public:
+    static const Tribool fawse;
+    static const Tribool twue;
+    static const Tribool maybe;
 
-[[nodiscard]] [[gnu::always_inline]] constexpr Tribool operator&&(Tribool x, Tribool y) noexcept
-{
-    return Tribool(static_cast<signed char>(x) & static_cast<signed char>(y));
-}
+    constexpr Tribool(bool x) noexcept
+        : underlying(-int(x))
+    {
+        BIT_MANIPULATION_ASSUME(x >= -1 && x <= 1);
+    }
 
-[[nodiscard]] [[gnu::always_inline]] constexpr Tribool operator&&(Tribool x, bool y) noexcept
-{
-    return Tribool(static_cast<signed char>(x) & -static_cast<signed char>(y));
-}
+    [[nodiscard]] [[gnu::always_inline]] friend constexpr bool operator==(const Tribool&,
+                                                                          const Tribool&)
+        = default;
 
-[[nodiscard]] [[gnu::always_inline]] constexpr Tribool operator&&(bool x, Tribool y) noexcept
-{
-    return y && x;
-}
+    [[nodiscard]] [[gnu::always_inline]] friend constexpr Tribool operator!(Tribool x) noexcept
+    {
+        BIT_MANIPULATION_ASSUME(x.underlying >= -1 && x.underlying <= 1);
+        return x == Tribool::maybe ? Tribool::maybe : Tribool(~x.underlying);
+    }
 
-[[nodiscard]] [[gnu::always_inline]] constexpr Tribool operator||(Tribool x, Tribool y) noexcept
-{
-    return Tribool(static_cast<signed char>(x) | static_cast<signed char>(y));
-}
+    [[nodiscard]] [[gnu::always_inline]] friend constexpr Tribool operator&&(Tribool x,
+                                                                             Tribool y) noexcept
+    {
+        BIT_MANIPULATION_ASSUME(x.underlying >= -1 && x.underlying <= 1);
+        BIT_MANIPULATION_ASSUME(y.underlying >= -1 && y.underlying <= 1);
+        return Tribool(x.underlying & y.underlying);
+    }
 
-[[nodiscard]] [[gnu::always_inline]] constexpr Tribool operator||(Tribool x, bool y) noexcept
-{
-    return Tribool(static_cast<signed char>(x) | -static_cast<signed char>(y));
-}
+    [[nodiscard]] [[gnu::always_inline]] friend constexpr Tribool operator||(Tribool x,
+                                                                             Tribool y) noexcept
+    {
+        BIT_MANIPULATION_ASSUME(x.underlying >= -1 && x.underlying <= 1);
+        BIT_MANIPULATION_ASSUME(y.underlying >= -1 && y.underlying <= 1);
+        return Tribool(x.underlying | y.underlying);
+    }
 
-[[nodiscard]] [[gnu::always_inline]] constexpr Tribool operator||(bool x, Tribool y) noexcept
-{
-    return y && x;
-}
+    [[nodiscard]] [[gnu::always_inline]] friend constexpr Tribool equals(Tribool x,
+                                                                         Tribool y) noexcept
+    {
+        BIT_MANIPULATION_ASSUME(x.underlying >= -1 && x.underlying <= 1);
+        BIT_MANIPULATION_ASSUME(y.underlying >= -1 && y.underlying <= 1);
+        return x == Tribool::maybe || y == Tribool::maybe ? Tribool::maybe
+                                                          : Tribool(~(x.underlying ^ y.underlying));
+    }
 
-[[nodiscard]] [[gnu::always_inline]] constexpr bool operator==(Tribool x, bool y) noexcept
-{
-    return static_cast<signed char>(x) == -static_cast<signed char>(y);
-}
+    [[nodiscard]] [[gnu::always_inline]] friend constexpr Tribool not_equals(Tribool x,
+                                                                             Tribool y) noexcept
+    {
+        BIT_MANIPULATION_ASSUME(x.underlying >= -1 && x.underlying <= 1);
+        BIT_MANIPULATION_ASSUME(y.underlying >= -1 && y.underlying <= 1);
+        return x == Tribool::maybe || y == Tribool::maybe ? Tribool::maybe
+                                                          : Tribool(x.underlying ^ y.underlying);
+    }
+};
 
-[[nodiscard]] [[gnu::always_inline]] constexpr Tribool equals(Tribool x, Tribool y) noexcept
-{
-    return x == Tribool::maybe || y == Tribool::maybe
-        ? Tribool::maybe
-        : Tribool(~(static_cast<signed char>(x) ^ static_cast<signed char>(y)));
-}
-
-[[nodiscard]] [[gnu::always_inline]] constexpr Tribool equals(Tribool x, bool y) noexcept
-{
-    return x == Tribool::maybe
-        ? Tribool::maybe
-        : Tribool(~(static_cast<signed char>(x) ^ static_cast<signed char>(y)));
-}
-
-[[nodiscard]] [[gnu::always_inline]] constexpr Tribool equals(bool x, Tribool y) noexcept
-{
-    return equals(y, x);
-}
-
-[[nodiscard]] [[gnu::always_inline]] constexpr Tribool not_equals(Tribool x, Tribool y) noexcept
-{
-    return x == Tribool::maybe || y == Tribool::maybe
-        ? Tribool::maybe
-        : Tribool(static_cast<signed char>(x) ^ static_cast<signed char>(y));
-}
-
-[[nodiscard]] [[gnu::always_inline]] constexpr Tribool not_equals(Tribool x, bool y) noexcept
-{
-    return x == Tribool::maybe ? Tribool::maybe
-                               : Tribool(static_cast<signed char>(x) ^ static_cast<signed char>(y));
-}
-
-[[nodiscard]] [[gnu::always_inline]] constexpr Tribool not_equals(bool x, Tribool y) noexcept
-{
-    return not_equals(y, x);
-}
+inline constexpr Tribool Tribool::fawse { 0 };
+inline constexpr Tribool Tribool::twue { -1 };
+inline constexpr Tribool Tribool::maybe { 1 };
 
 } // namespace bit_manipulation
 
