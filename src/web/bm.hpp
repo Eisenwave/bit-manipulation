3221849f3e03b7394abfdfd8b0348855d980f37c
Eisenwave <me@eisenwave.net>
2024-12-09 13:15:40 +0100

    [web] add basic WASM library stub

---
+++ src/web/bm.hpp
@@ -0,0 +1,15 @@
+#ifndef BIT_MANIPULATION_WEB_BM_HPP
+#define BIT_MANIPULATION_WEB_BM_HPP
+
+#include "common/config.hpp"
+
+#ifdef BIT_MANIPULATION_EMSCRIPTEN
+#include <emscripten.h>
+
+extern "C" {
+//
+}
+
+#endif
+
+#endif
