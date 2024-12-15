4f826cbdf020b189aa9d6d0b81f501435c906630
Eisenwave <me@eisenwave.net>
2024-12-15 18:30:53 +0100

    [bmd] add Data_HTML_Token_Consumer

--- src/bmd/html/token_consumer.hpp
+++ src/bmd/html/token_consumer.hpp
@@ -12,9 +12,40 @@ namespace bit_manipulation::bmd {
 /// token type to write.
 /// The token type can be ignored, but may be used to output with syntax highlighting.
 struct HTML_Token_Consumer {
+
+    /// @brief Consumes a single character `c` with the given `type`.
     virtual bool write(char c, HTML_Token_Type type) = 0;
+
+    /// @brief Consumes `n` repetitions of a character `c` with the given `type`.
+    /// This is typically used for indentation.
     virtual bool write(char c, Size n, HTML_Token_Type type) = 0;
+
+    /// @brief Consumes a string `s` with the given `type`.
     virtual bool write(std::string_view s, HTML_Token_Type type) = 0;
+};
+
+/// @brief A convenience base class for token consumer which ignore the
+/// given token types and consume only the character data.
+struct Data_HTML_Token_Consumer : HTML_Token_Consumer {
+
+    bool write(char c, HTML_Token_Type) final
+    {
+        return write(c);
+    }
+
+    bool write(char c, Size n, HTML_Token_Type) final
+    {
+        return write(c, n);
+    }
+
+    bool write(std::string_view s, HTML_Token_Type) final
+    {
+        return write(s);
+    }
+
+    virtual bool write(char c) = 0;
+    virtual bool write(char c, Size n) = 0;
+    virtual bool write(std::string_view s) = 0;
 };
 
 } // namespace bit_manipulation::bmd
