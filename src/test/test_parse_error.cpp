84c5d3681485392831b5b524de26c1a5566d2dc6
Eisenwave <me@eisenwave.net>
2024-12-21 15:41:04 +0100

    [test] put parse error tests in separate file

---
+++ src/test/test_parse_error.cpp
@@ -0,0 +1,57 @@
+#include <gtest/gtest.h>
+
+#include "bms/parsing/grammar.hpp"
+
+#include "test/program_file_testing.hpp"
+
+namespace bit_manipulation {
+namespace {
+
+TEST(BMS_Parse_Error, global_let)
+{
+    constexpr Parse_Error_Expectations expectations //
+        { .line = 1, .token_type = bms::Token_Type::keyword_let };
+    EXPECT_TRUE(test_for_diagnostic("parse_error/global_let.bms", expectations));
+}
+
+TEST(BMS_Parse_Error, let_let)
+{
+    constexpr Parse_Error_Expectations expectations //
+        { .rule = bms::Grammar_Rule::let_declaration,
+          .line = 2,
+          .token_type = bms::Token_Type::keyword_let };
+    EXPECT_TRUE(test_for_diagnostic("parse_error/let_let.bms", expectations));
+}
+
+TEST(BMS_Parse_Error, let_no_type_no_init)
+{
+    constexpr Parse_Error_Expectations expectations //
+        { .line = 2, .token_type = bms::Token_Type::semicolon };
+    EXPECT_TRUE(test_for_diagnostic("parse_error/let_no_type_no_init.bms", expectations));
+}
+
+TEST(BMS_Parse_Error, missing_semicolon_eof)
+{
+    constexpr Parse_Error_Expectations expectations //
+        { .line = 1, .token_type = bms::Token_Type::eof };
+    EXPECT_TRUE(test_for_diagnostic("parse_error/missing_semicolon_eof.bms", expectations));
+}
+
+TEST(BMS_Parse_Error, nameless_function)
+{
+    constexpr Parse_Error_Expectations expectations //
+        { .rule = bms::Grammar_Rule::function_declaration,
+          .line = 1,
+          .token_type = bms::Token_Type::left_parenthesis };
+    EXPECT_TRUE(test_for_diagnostic("parse_error/nameless_function.bms", expectations));
+}
+
+TEST(BMS_Parse_Error, unbalanced_parentheses)
+{
+    constexpr Parse_Error_Expectations expectations //
+        { .line = 1, .token_type = bms::Token_Type::assign };
+    EXPECT_TRUE(test_for_diagnostic("parse_error/unbalanced_parentheses.bms", expectations));
+}
+
+} // namespace
+} // namespace bit_manipulation
