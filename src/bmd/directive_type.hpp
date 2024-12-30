00396c3346355b91d8a68b687569103a28298089
Eisenwave <me@eisenwave.net>
2024-12-30 14:21:26 +0100

    [bmd] nodiscard

--- src/bmd/directive_type.hpp
+++ src/bmd/directive_type.hpp
@@ -80,7 +80,8 @@ enum struct Directive_Environment : Default_Underlying {
     list,
 };
 
-constexpr std::strong_ordering operator<=>(Directive_Type x, Directive_Type y) noexcept
+[[nodiscard]] constexpr std::strong_ordering operator<=>(Directive_Type x,
+                                                         Directive_Type y) noexcept
 {
     return static_cast<Default_Underlying>(x) <=> static_cast<Default_Underlying>(y);
 }
@@ -89,7 +90,7 @@ constexpr std::strong_ordering operator<=>(Directive_Type x, Directive_Type y) n
 /// any block content, such as `\br` or `\hr`.
 /// @param type the directive type
 /// @return `true` if the corresponding directive must be empty, `false` otherwise.
-inline bool directive_type_must_be_empty(Directive_Type type)
+[[nodiscard]] inline bool directive_type_must_be_empty(Directive_Type type)
 {
     using enum Directive_Type;
     switch (type) {
@@ -104,19 +105,19 @@ inline bool directive_type_must_be_empty(Directive_Type type)
 /// `block`.
 /// @param type the directive type
 /// @return The corresponding `Formatting_Style`.
-Formatting_Style directive_type_formatting_style(Directive_Type type);
+[[nodiscard]] Formatting_Style directive_type_formatting_style(Directive_Type type);
 
-Directive_Content_Type directive_type_content_type(Directive_Type type);
+[[nodiscard]] Directive_Content_Type directive_type_content_type(Directive_Type type);
 
-Directive_Environment directive_type_environment(Directive_Type type);
+[[nodiscard]] Directive_Environment directive_type_environment(Directive_Type type);
 
-inline bool directive_content_allows_directives(Directive_Content_Type type)
+[[nodiscard]] inline bool directive_content_allows_directives(Directive_Content_Type type)
 {
     return type != Directive_Content_Type::nothing //
         && type != Directive_Content_Type::text_span;
 }
 
-inline bool directive_content_allows_only_directives(Directive_Content_Type type)
+[[nodiscard]] inline bool directive_content_allows_only_directives(Directive_Content_Type type)
 {
     return type == Directive_Content_Type::meta || type == Directive_Content_Type::list;
 }
@@ -127,9 +128,11 @@ inline bool directive_content_allows_only_directives(Directive_Content_Type type
 /// @param environment the environment that the inner directive is allowed in
 /// @return `true` if the directive with the given `environment` is allowed in a
 /// directive with the given `content`, `false` otherwise.
-bool directive_content_allows(Directive_Content_Type content, Directive_Environment environment);
+[[nodiscard]] bool directive_content_allows(Directive_Content_Type content,
+                                            Directive_Environment environment);
 
-inline bool directive_type_allowed_in(Directive_Type type, Directive_Content_Type content)
+[[nodiscard]] inline bool directive_type_allowed_in(Directive_Type type,
+                                                    Directive_Content_Type content)
 {
     return directive_content_allows(content, directive_type_environment(type));
 }
@@ -138,17 +141,18 @@ inline bool directive_type_allowed_in(Directive_Type type, Directive_Content_Typ
 /// For example, `directive_type_by_id("b")` yields `bold`.
 /// @param directive_id the directive identifier
 /// @return The corresponding `Directive_Type` if one exists, `std::nullopt` otherwise.
-std::optional<Directive_Type> directive_type_by_id(std::string_view directive_id) noexcept;
+[[nodiscard]] std::optional<Directive_Type>
+directive_type_by_id(std::string_view directive_id) noexcept;
 
 /// @brief Returns the corresponding HTML tag string for a given `Directive_Type`.
 /// @param type the directive type
 /// @return The corresponding HTML tag.
-std::string_view directive_type_tag(Directive_Type type);
+[[nodiscard]] std::string_view directive_type_tag(Directive_Type type);
 
 /// @brief Checks whether the corresponding directive is an "HTML passthrough
 /// directive". That is, a directive which has no unique rules and simply transforms
 /// into an HTML tag. For example, `\b{...}` directly translates into `<b>...</b>`.
-inline bool directive_type_is_html_passthrough(Directive_Type type)
+[[nodiscard]] inline bool directive_type_is_html_passthrough(Directive_Type type)
 {
     return type <= Directive_Type::unordered_list;
 }
