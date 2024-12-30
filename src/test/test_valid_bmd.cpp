722dd399e3968865682ca6d218a6f778b1745465
Eisenwave <me@eisenwave.net>
2024-12-30 14:22:24 +0100

    [test] add basic BMD tests

--- src/test/test_valid_bmd.cpp
+++ src/test/test_valid_bmd.cpp
@@ -10,5 +10,20 @@ TEST(Valid_BMD, empty)
     EXPECT_TRUE(test_for_success("bmd/empty.bmd", BMD_Stage::process));
 }
 
+TEST(Valid_BMD, hello_code)
+{
+    EXPECT_TRUE(test_for_success("bmd/hello_code.bmd", BMD_Stage::process));
+}
+
+TEST(Valid_BMD, hello_directive)
+{
+    EXPECT_TRUE(test_for_success("bmd/hello_directive.bmd", BMD_Stage::process));
+}
+
+TEST(Valid_BMD, hello_world)
+{
+    EXPECT_TRUE(test_for_success("bmd/hello_world.bmd", BMD_Stage::process));
+}
+
 } // namespace
 } // namespace bit_manipulation
