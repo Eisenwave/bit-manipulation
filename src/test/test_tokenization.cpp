be00781ded3df62827f39e903f9722292faf13db
Eisenwave <me@eisenwave.net>
2025-02-06 15:56:47 +0100

    [test] test whether all tokens can be tokenized

--- src/test/test_tokenization.cpp
+++ src/test/test_tokenization.cpp
@@ -23,6 +23,11 @@ struct Textual_Token {
     bms::Token_Type type;
     std::string_view text = bms::token_type_code_name(type);
 
+    [[nodiscard]] constexpr bool empty() const
+    {
+        return text.empty();
+    }
+
     [[maybe_unused]] [[nodiscard]] friend std::strong_ordering operator<=>(const Textual_Token&,
                                                                            const Textual_Token&)
         = default;
@@ -130,18 +135,99 @@ TEST(Tokenize, token_length_consistent)
 
 TEST(Tokenize, regression_1)
 {
+    using enum bms::Token_Type;
+
     static const Textual_Token expected[] {
-        { bms::Token_Type::keyword_return },    { bms::Token_Type::left_parenthesis },
-        { bms::Token_Type::logical_not },       { bms::Token_Type::identifier, "x" },
-        { bms::Token_Type::right_parenthesis }, { bms::Token_Type::logical_and },
-        { bms::Token_Type::left_parenthesis },  { bms::Token_Type::identifier, "y" },
-        { bms::Token_Type::logical_or },        { bms::Token_Type::logical_not },
-        { bms::Token_Type::identifier, "y" },   { bms::Token_Type::right_parenthesis },
-        { bms::Token_Type::semicolon },
+        { keyword_return },    { left_parenthesis }, { logical_not },      { identifier, "x" },
+        { right_parenthesis }, { logical_and },      { left_parenthesis }, { identifier, "y" },
+        { logical_or },        { logical_not },      { identifier, "y" },  { right_parenthesis },
+        { semicolon },
     };
 
     std::pmr::monotonic_buffer_resource memory;
     std::optional<Tokenized_Program> actual = tokenize("syntax/tokenize_regression.bms", &memory);
+
+    ASSERT_TRUE(actual);
+    ASSERT_TRUE(actual->check_equals(expected));
+}
+
+TEST(Tokenize, all_tokens)
+{
+    using enum bms::Token_Type;
+
+    // clang-format off
+    static const Textual_Token expected[] {
+        { identifier, "identifier" },
+        { left_parenthesis },
+        { right_parenthesis },
+        { decimal_literal, "12345" },
+        { octal_literal, "0242" },
+        { hexadecimal_literal, "0xff" },
+        { binary_literal, "0b11" },
+        { string_literal, "\"abc\"" },
+        { left_brace },
+        { right_brace },
+        { block_comment, "/* ... */" },
+        { line_comment, "// ..." },
+        { assign },
+        { equals },
+        { not_equals },
+        { plus },
+        { minus },
+        { multiplication },
+        { division },
+        { remainder },
+        { less_than },
+        { greater_than },
+        { less_or_equal },
+        { greater_or_equal },
+        { shift_left },
+        { shift_right },
+        { bitwise_and },
+        { bitwise_or },
+        { bitwise_not },
+        { bitwise_xor },
+        { logical_and },
+        { logical_or },
+        { logical_not },
+        { right_arrow },
+        { double_right_arrow },
+        { dot },
+        { colon },
+        { comma },
+        { at },
+        { semicolon },
+        { keyword_as },
+        { keyword_let },
+        { keyword_const },
+        { keyword_function },
+        { keyword_while },
+        { keyword_if },
+        { keyword_else },
+        { keyword_uint },
+        { keyword_int },
+        { keyword_bool },
+        { keyword_void },
+        { keyword_nothing },
+        { keyword_requires },
+        { keyword_return },
+        { keyword_break },
+        { keyword_continue },
+        { keyword_true },
+        { keyword_false },
+        { keyword_static_assert },
+     };
+    // clang-format on
+
+    // Note that eof is deliberately not in the array because it is the only type of token that is
+    // never produced by the tokenizer.
+    static_assert(std::size(expected) == Size(keyword_static_assert),
+                  "Incomplete test expectations. Update the array above with any missing values.");
+    // This assertion would fail only as the result of developer error.
+    BIT_MANIPULATION_ASSERT(std::ranges::none_of(expected, &Textual_Token::empty));
+
+    std::pmr::monotonic_buffer_resource memory;
+    std::optional<Tokenized_Program> actual = tokenize("syntax/all_tokens.bms", &memory);
 
     ASSERT_TRUE(actual);
     ASSERT_TRUE(actual->check_equals(expected));
